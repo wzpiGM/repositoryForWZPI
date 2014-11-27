021eaa5422b595ff928dc38c351e6685ab066285
GenMyModel <help@genmymodel.com>
2014-11-27 20:00:39 +0000

    commit by GenMyModel using genmymodel: v20141127_200038

---
+++ src/main/java/model/Restauracja.java
@@ -0,0 +1,53 @@
+package model;
+import java.util.Set;
+import java.util.HashSet;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Restauracja
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String nazwaRestauracji;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public Set<Zamowienie> zamowienie;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Restauracja(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void operation() {
+		// TODO implement me	
+	}
+	
+}
+
